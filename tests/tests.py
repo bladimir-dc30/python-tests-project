82cc3939c3f0c60c884a76445d0f5cfc399fe308
bladoumerc <bdoumerc15@outlook.com>
2025-05-01 21:22:57 -0600

    feat/add new tests

---
+++ tests/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from src.calculator import *
+
+
+class CalculatorTests(unittest.TestCase):
+    def test_add(self):
+        assert add(5, 10) == 15
+
+    def test_subtract(self):
+        assert subtract(10, 5) == 5
+
+    def test_division(self):
+        assert division(20, 4) == 5
+        
+    def test_multiplication(self):
+        assert multiplication(10, 5) == 50
